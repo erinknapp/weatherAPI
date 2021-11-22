95aff9c26577f4ebba7ea239cc32be556376517d
Erin Knapp <erin.knapp@gmail.com>
2021-11-21 19:30:10 -0500

    Added styles to dashbaord

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,61 @@
-var formEl = $("#city-form");
-var weatherEl = $("weather-details");
+// Global variables
+var cityFormEl = document.querySelector("#city-form");
+var historyListEl = document.querySelector("#history-list");
+var weatherSearchTerm = document.querySelector ("#weather-search-term");
+var weatherContainer = document.querySelector("#weather-container");
+var cityInputEl = document.querySelector("#city-input");
 
-var handleFormSubmit = function(event) {
+
+// Function for form submission and error checking
+var formSubmitHandler = function (event) {
     //prevent default reloading webpage
     event.preventDefault();
-  
-    var cityInput= formEl.val();
-  
-    if (!cityInput) {
-      console.log('You need to enter a city name!');
-      return;
+
+    var city = cityInputEl.value.trim();
+
+    // error check
+    if (city) {
+        getCity(city);
+        cityFormEl.textContent = "";
+        cityInputEl = "";
+    } else (
+        alert('Please enter a city name')
+    )
+};
+
+//button click event
+var buttonClickHandler = function (event) {
+    var name = event.target.getAttribute('data-name');
+    if (name) {
+        getCity(name);
+
+        weatherContainer.textContent = "";
     }
-  
-    printSkills(nameInput, dateInput);
-  
-    // resets form
-    nameInputEl.val('');
-    dateInputEl.val('');
-  };
+};
+
+// Function to capture city data for use in API
+var getCity = function (city) {
+    var apiURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=1f193a6993fbc7775567c85ed6ca33da";
+
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayWeather(data.items, );
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+                alert('Unable to connect to OpenWeather');
+        });
+};
+
+// Function to display results based on city search
+var displayWeather = function () {
+
+};
+
+cityFormEl.addEventListener('submit', formSubmitHandler);
